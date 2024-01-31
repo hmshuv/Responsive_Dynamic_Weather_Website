0a2344f2f34c801bef1492c861adc8f8a2b52150
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-01-31 18:50:26 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const port = process.env.PORT || 7000; //used for deploying the code
 
+//public static path
+
+const static_path = path.join((__dirname), "../public/");
+
+app.use(express.static(static_path));
+
+//routing
 app.get("", (req, res)=>{
     res.send("Welcome to himanshu's weather project");
 })
