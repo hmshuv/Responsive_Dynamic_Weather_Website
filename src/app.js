0ccdb8c75c1bc355203f5a543fd353ad1b7d8f8c
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-06 23:48:03 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const port = process.env.PORT || 7000; //Uused for deploying the code
+const port = process.env.PORT || 7000;   //Uused for deploying the code
 
 //public static path
 
@@ -14,7 +14,7 @@ app.get("/", (req, res)=>{
     res.send("Welcome to himanshu's weather project");
 })
 app.get("/about", (req, res)=>{
-    res.send("Welcome to himanshu's weather project About Page");
+    res.send("Welcome to Himanshu's Weather project About Page");
 })
 app.get("/weather", (req, res)=>{
     res.send("Welcome to himanshu's weather project weather Page");
@@ -23,4 +23,4 @@ app.get("*", (req, res)=>{
     res.send("404!!!! OOPs page not found :(");
 })
 
-app.listen(port, ()=>console.log(`port ${port} is listening`));
+app.listen(port, ()=> console.log(`port ${port} is listening`));
